cf2271a99baaaf06e71d0315eefff90d2278fd03
RobbMolina <jose.molina@iest.edu.mx>
2025-11-10 17:55:05 -0600

    cambios de la api

---
+++ API/PokemonListViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  PokemonListViewModel.swift
+//  API
+//
+//  Created by win603 on 07/11/25.
+//
+
+import Foundation
+
+class PokemonListViewModel : ObservableObject {
+    @Published var pokemon: Pokemon
+    
+    init(){
+        self.pokemon = Pokemon(count: -1, next: "" , previous: "" , results: [])
+    }
+    
+    func getPokemonList(){
+        let endPoint : String = "https://pokeapi.co/api/v2/pokemon"
+        guard let apiURL = URL(string: endPoint) else{
+            fatalError("Url no válida o no definida")
+        }
+        var urlRequest = URLRequest(url: apiURL)
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "GET"
+        /*Configuración de Cabeceras(Headers) en caso que la petición lo demande:
+         urlRequest.addValue("token123", forHTTPHeaderField: "Authorization")
+         urlRequest.addValue("ios", forHTTPHeaderField: "User-Agent")
+         */
+        /*Ejemplo de inicialización y envío parametros POST
+         let parameters: [String: Any] = [
+         "id_trainer" : 1,
+         "name" : "Ash",
+         "champion" : true,
+         ]
+         
+         let jsonData = try? JSONSerialization.data(withJSONObject: params)
+         urlRequest.httpBody = jsonData
+         */
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest, completionHandler: { data, response, error -> Void in
+            
+            if let error = error {
+                print("Error: " + error.localizedDescription)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print("Error: Incapaz de conectar o recibir respuesta del servidor")
+                return
+            }
+            
+            
+            if response.statusCode == 200 {
+                
+                guard let data = data else {
+                    print("Respuesta vacía")
+                    return
+                }
+                
+                /*AQUÍ NUESTRA LOGICA para procesar la respuesta
+                 */
+                
+                DispatchQueue.main.async {
+                                    do {
+                                        let decoded = try JSONDecoder().decode(Pokemon.self, from: data)
+                                   
+                                        self.pokemon = decoded
+                                    } catch let error {
+                                        print("Error al decodificar JSON \(error)")
+                                        return
+                                    }
+                                }
+
+                
+            }else{
+                print("Status diferente a 200. Ejemplos: 500 - Internal server error, 400 - Not found , etc.")
+                return
+            }
+            
+        })
+        task.resume()
+    }
+}
+
+
